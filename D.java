bd411b7016360d833f4f5540a688bdc286ffde79
Witor Bueno <witor.bueno@estudante.ufla.br>
2019-04-17 20:30:17 -0300

    Nova classe D

---
+++ D.java
@@ -0,0 +1,29 @@
+
+public class D {
+	private String D1;
+	private  Int D2;
+	
+	
+	public int getD1() {
+		return D1;
+	}
+	public void setD1(int D1) {
+		C1 = C1;
+	}
+	
+	public float getD2() {
+		return D2;
+	}
+	
+	public void setB2(float D2) {
+		D2 = D2;
+	}
+	public void MD1(){
+		System.out.println("MD1");
+	}
+	public void MD2(){
+		System.out.println("MD2");
+	}
+
+
+}		//WITOR ANGELO BUENO
