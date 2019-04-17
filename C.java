733effe70879bcc5caef74fbe8b0ed7d7f862f88
Witor Bueno <witor.bueno@estudante.ufla.br>
2019-04-17 20:15:33 -0300

    Criacao da Classe C

---
+++ C.java
@@ -0,0 +1,29 @@
+
+public class C {
+	private String C1;
+	private  Int C2;
+	
+	
+	public int getC1() {
+		return B1;
+	}
+	public void setC1(int C1) {
+		C1 = C1;
+	}
+	
+	public float getC2() {
+		return C2;
+	}
+	
+	public void setB2(float C2) {
+		C2 = C2;
+	}
+	public void MC1(){
+		System.out.println("MC1");
+	}
+	public void MC2(){
+		System.out.println("MC2");
+	}
+
+
+}		//WITOR ANGELO BUENO
