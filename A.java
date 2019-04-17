58b1d04b6e7afe694ee7cd6f3fc4aeb440987431
witorbueno <49721429+witorbueno@users.noreply.github.com>
2019-04-17 16:13:17 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,31 @@
+
+public class A {
+	private int A1;  
+	private float A2;
+	
+	
+	public int getA1() {
+		return A1;
+	}
+	
+	public void setA1(int a1) {
+		A1 = a1;
+	}
+	
+	public float getA2() {
+		return A2;
+	}
+	
+	public void setA2(float a2) {
+		A2 = a2;
+	}
+	
+	public void MA1(){
+		System.out.println("MA1");
+	}
+	
+	public void MA2(){
+		System.out.println("MA2");
+	
+} // WITOR ANGELO BUENO
+}
---
+++ B.java
@@ -0,0 +1,28 @@
+
+public class B {
+	private int B1;
+	private float B2;
+	
+	
+	public int getB1() {
+		return B1;
+	}
+	public void setB1(int b1) {
+		B1 = b1;
+	}
+	
+	public float getB2() {
+		return B2;
+	}
+	
+	public void setB2(float b2) {
+		B2 = b2;
+	}
+	public void MB1(){
+		System.out.println("MB1");
+	}
+	public void MB2(){
+		System.out.println("MB2");
+	}
+
+}		//WITOR ANGELO BUENO
